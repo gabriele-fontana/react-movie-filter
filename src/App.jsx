6314570160a0bc6d9f8b57537f9cd03970a73acf
gabriele-fontana <gabrielefontana.2001@gmail.com>
2026-03-16 20:38:28 +0100

    add input, setState, onChange and render filteredMovies

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,25 @@ const movies = [
 
 function App() {
   
-  
-  
+  const [search, setSearch] = useState('')
+  const filteredMovies = movies.filter(movie =>
+    movie.genre.includes(search)
+  )
   
   
   
   return (
     <div>
       <h1>Movies</h1>
+      <input
+        type="text"
+        placeholder="search"
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
 
       <ul>
-        {movies.map((movie) => (
+        {filteredMovies.map((movie) => (
           <li>
             <h3>{movie.title}</h3>
           </li>
