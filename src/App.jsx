0c4e36c4d1660e10239f0b2331be58a1ee79165e
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-03-16 23:15:54 +0100

    create a component for movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import { useState, useEffect } from "react"
-
-const movies = [
-  { title: 'Inception', genre: 'Fantascienza' },
-  { title: 'Il Padrino', genre: 'Thriller' },
-  { title: 'Titanic', genre: 'Romantico' },
-  { title: 'Batman', genre: 'Azione' },
-  { title: 'Interstellar', genre: 'Fantascienza' },
-  { title: 'Pulp Fiction', genre: 'Thriller' },
-]
-
+import movies from "../components/Movies"
 
 function App() {
   
