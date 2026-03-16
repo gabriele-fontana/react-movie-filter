ff6240383140d9d7c8200ce96d04036996a0864b
gabriele-fontana <gabrielefontana.2001@gmail.com>
2026-03-16 11:34:31 +0100

    add titles list

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
+import { useState, useEffect } from "react"
 
-
-
+const movies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 
 function App() {
   
+  
+  
+  
+  
+  
+  return (
+    <div>
+      <h1>Movies</h1>
 
-  
-  
-  
-  
-  
-  return 
 
+      <ul>
+        {movies.map((movie) => (
+          <li>
+            <h3>{movie.title}</h3>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 
 
 
